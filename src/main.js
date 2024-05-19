cc0cbba1d96d503b06403cfd1b21a5c4acf4aa24
charlie <2295509496@qq.com>
2024-05-19 16:31:50 +0800

    项目基本页面完成

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import '@/assets/css/global.css'
 // 引入element-plus: 1. 引入组件 2. 引入样式 3. 使用 .use(ElementPlus)
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 国际化: 引入并与ElementPlus整合关联
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 // main.js用于引入资源(css/组件等)，同时也是创建 App 挂载 #app，引入 ./router ./store 等资源的所在
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus, {locale: zhCn}).mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,69 @@
 <template>
-  <!--去掉 class="home"-->
   <div>
-    <!--<img alt="Vue logo" src="../assets/logo.png">-->
-    <!--
-    1. 在 HomeView.vue 中引入 'src/components/HelloWorld.vue'
-    -->
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <!--新增按钮-->
+    <div style="margin: 10px 0">
+      <el-button type="primary">新增</el-button>
+      <el-button>其它</el-button>
+    </div>
+    <!--引入搜索框-->
+    <div style="margin: 10px 0">
+      <el-input v-model="search" placeholder="请输入关键字" style="width: 30%"></el-input>
+      <el-button style="margin-left: 10px" type="primary">查询</el-button>
+    </div>
+
     <!--将表格数据(即后端数据)放在HomeView.vue中-->
     <el-table :data="tableData" stripe style="width: 100%">
-      <el-table-column prop="date" label="日期"/>
+      <el-table-column sortable prop="date" label="日期"/>
       <el-table-column prop="name" label="姓名"/>
       <el-table-column prop="address" label="地址"/>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template #default="scope">
+          <!--自定义内容需要使用该行数据时，声明 #default="scope"，再通过 scope.row 获取当前行数据-->
+          <el-button type="text" @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button type="text">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
-// 2. @ 表示 /src 目录
-// 3. 给 HelloWorld.vue 文件取名 HelloWorld
-// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   // 导出，这样在HomeView.vue就可以使用HelloWorld组件
   name: 'HomeView',
-  components: {
-    // HelloWorld
+  components: {},
+  data() { // 数据部分
+    return {
+      search: '',
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: '狗熊岭',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Jack',
+          address: '四季村',
+        },
+        {
+          date: '2016-05-04',
+          name: 'Kim',
+          address: '团结屯',
+        },
+        {
+          date: '2016-05-01',
+          name: 'Light',
+          address: '龙虎山',
+        },
+      ]
+    }
+  },
+  methods: {  // 方法
+    handleEdit() {
+
+    }
   }
 }
 </script>
