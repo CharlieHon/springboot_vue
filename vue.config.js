6e8f62e351e2a9f40ef01a91d0a96ac250f7ec70
charlie <2295509496@qq.com>
2024-05-30 21:45:48 +0800

    配置代理处理跨域请求

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,16 @@ module.exports = defineConfig({
 })
 module.exports = {
     devServer: {
-        port: 10001     // 启动端口
+        port: 10001,     // 启动端口
+        proxy: {            // 设置代理，处理跨域问题
+            '/api': {       // 设置拦截器，拦截器格式：斜杠+拦截器名字
+                target: 'http://localhost:9090',    // 代理的目标地址，就是 /api 代替 http://localhost/9090
+                changeOrigin: true,     // 是否设置同源，true实现跨域
+                pathRewrite: {          // 路径重写
+                    '/api': ''          // 选择忽略拦截器里面的单词
+                }
+            }
+        }
     }
 }
+// 修改了配置文件，需要重启项目才能生效！
